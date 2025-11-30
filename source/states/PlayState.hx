88c384bfe87ded9cc2fbb3475d231b65112fbf2c
Prohack101010 <28umutmehmet28@gmail.com>
2025-11-30 05:39:46 +0000

    fck u haxe

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -6654,7 +6654,7 @@ class PlayState extends MusicBeatState
 
 	//I don't need this anymore because Hitboxes can returnable to any keys
 	public static function checkHBoxPress(button:String, type = 'justPressed') {
-		var buttonObject:MobileButton;
+		var buttonObject:MobileButton = null;
 		if (MusicBeatState.getState().hitbox != null) buttonObject = MusicBeatState.getState().hitbox.getButtonFromName(button);
 		if (buttonObject != null) return Reflect.getProperty(buttonObject, type);
 		return false;
